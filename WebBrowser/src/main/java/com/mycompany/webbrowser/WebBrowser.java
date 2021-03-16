848575eaef4af7dff402fff52ccb0b9f3d057b3f
Himanshu Verkiya <hiverkiya@gmail.com>
2021-03-16 17:22:58 +0530

    Adding JavaFX Files

--- WebBrowser/src/main/java/com/mycompany/webbrowser/WebBrowser.java
+++ WebBrowser/src/main/java/com/mycompany/webbrowser/WebBrowser.java
@@ -1,22 +1,39 @@
-package com.mycompany.webbrowser;
-import javax.swing.*;
-import javafx.embed.swing.JFXPanel;
 /**
  *
  * @author hiverkiya
  *
  */
-public class WebBrowser extends JFrame {
+package com.mycompany.webbrowser;
+import javax.swing.*;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.*;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class WebBrowser extends JFrame implements Runnable{
+
+    private WebEngine webEngine;
     private JFXPanel panel;
-    WebBrowser(){
-        setTitle("Web Browser");
-        setBounds(0,0,1920,1080);
+
+    public void run() {
+        setTitle("Search");
         setVisible(true);
+        setBounds(0,0,1950, 1080);
+
         panel = new JFXPanel();
         add(panel);
+
+        Platform.runLater(() -> {
+            WebView view = new WebView();
+            view.getEngine().load("http://www.google.com");
+
+            panel.setScene(new Scene(view));
+        });
     }
-    public static void main(String[] args)
-    {
-        new WebBrowser();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new WebBrowser());
     }
+
 }
