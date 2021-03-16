e03e9e371e7f8ce981f2d7a273cc71e6fd2bd42f
Himanshu Verkiya <hiverkiya@gmail.com>
2021-03-16 16:29:31 +0530

    Adding JavaFX

--- WebBrowser/src/main/java/com/mycompany/webbrowser/WebBrowser.java
+++ WebBrowser/src/main/java/com/mycompany/webbrowser/WebBrowser.java
@@ -1,15 +1,19 @@
 package com.mycompany.webbrowser;
 import javax.swing.*;
+import javafx.embed.swing.JFXPanel;
 /**
  *
  * @author hiverkiya
+ *
  */
 public class WebBrowser extends JFrame {
-    
+    private JFXPanel panel;
     WebBrowser(){
         setTitle("Web Browser");
         setBounds(0,0,1920,1080);
         setVisible(true);
+        panel = new JFXPanel();
+        add(panel);
     }
     public static void main(String[] args)
     {
